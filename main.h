ea5e1cd549cc3a11370c7a15c0991407239eb5a7
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2023-11-26 09:46:25 -0800

    cambio de variable en validate char y eliminacion de prototipos no usados en el main.h

--- main.h
+++ main.h
@@ -18,10 +18,7 @@ int _printf(const char *format, ...);
 int _print_char(va_list args);
 int _print_string(va_list args);
 int _print_format(const char *format, va_list args);
-int print_percentage(void);
-int _strlen(char *str);
-int _strlenc(const char *str);
-int _validate_char(const char *str_format);
+int _validate_char(char _type);
 int _print_specifiers(char format, va_list args);
 int _print_invalid(char prev_format, char format, int count);
 
